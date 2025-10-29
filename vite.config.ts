b5bef2bc7f46a133eaf6b611b544fd96b9495ae2
Maxxiiime <max2002guillon@gmail.com>
2025-10-29 14:06:26 +0100

    edit vite config 1

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build:
     {
-        outDir: '../dist', // Output in the dist/ folder
+        outDir: './dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
